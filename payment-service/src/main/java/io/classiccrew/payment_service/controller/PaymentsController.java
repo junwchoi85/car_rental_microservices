f98a6e43026b23d9b20ed48c4161ca55c970511d
Jun Hwan <junwchoi85@gmail.com>
2024-09-23 20:21:45 +1200

    Adding build-info

--- payment-service/src/main/java/io/classiccrew/payment_service/controller/PaymentsController.java
+++ payment-service/src/main/java/io/classiccrew/payment_service/controller/PaymentsController.java
@@ -3,11 +3,13 @@ package io.classiccrew.payment_service.controller;
 import org.springframework.web.bind.annotation.RestController;
 import io.classiccrew.payment_service.constants.PaymentsConstants;
 import io.classiccrew.payment_service.dto.ResponseDto;
+import io.classiccrew.payment_service.service.IPaymentsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,11 @@ import org.springframework.http.ResponseEntity;
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class PaymentsController {
+
+    private IPaymentsService iPaymentsService;
+
+    @Value("${build.version}")
+    private String buildVersion;
 
     @PostMapping("/request-payment")
     public ResponseEntity<ResponseDto> requestPayment(@RequestBody String entity) {
@@ -48,4 +55,10 @@ public class PaymentsController {
     public String ping() {
         return "pong";
     }
+
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildInfo() {
+        return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+    }
+
 }
