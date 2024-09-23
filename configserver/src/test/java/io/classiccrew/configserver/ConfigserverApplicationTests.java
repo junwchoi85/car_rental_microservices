8c6b0ee51c18de3c470f1e112ddc979a00047929
Jun Hwan <junwchoi85@gmail.com>
2024-09-23 21:35:48 +1200

    Introducing the Configserver

---
+++ configserver/src/test/java/io/classiccrew/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.classiccrew.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
