b17f2ff40a9825e0b72c7f7a8bcd681ec1bc8041
Jun Hwan <junwchoi85@gmail.com>
2024-09-24 11:00:46 +1200

    configuring payment service to locate configuration values from configserver

--- payment-service/src/main/java/io/classiccrew/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/io/classiccrew/payment_service/PaymentServiceApplication.java
@@ -2,12 +2,15 @@ package io.classiccrew.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import io.classiccrew.payment_service.dto.ContactInfoDto;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {ContactInfoDto.class})
 public class PaymentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceApplication.class, args);
+    }
 
 }
