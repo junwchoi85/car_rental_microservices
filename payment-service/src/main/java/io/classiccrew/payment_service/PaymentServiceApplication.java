847b946b53f57e0cf1f13ebf78a24b066a0c119a
Jun Hwan <junwchoi85@gmail.com>
2024-09-24 15:33:30 +1200

    enabling feign client

--- payment-service/src/main/java/io/classiccrew/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/io/classiccrew/payment_service/PaymentServiceApplication.java
@@ -3,10 +3,12 @@ package io.classiccrew.payment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import io.classiccrew.payment_service.dto.ContactInfoDto;
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = {ContactInfoDto.class})
+@EnableFeignClients
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
