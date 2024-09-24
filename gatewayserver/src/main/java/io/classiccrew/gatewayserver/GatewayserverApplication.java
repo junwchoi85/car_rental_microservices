f1236f85037d2c02e571212638550735dadcf011
Jun Hwan <junwchoi85@gmail.com>
2024-09-24 15:33:16 +1200

    Introducing Gateway server

---
+++ gatewayserver/src/main/java/io/classiccrew/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,25 @@
+package io.classiccrew.gatewayserver;
+
+import java.time.LocalDateTime;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routeConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes().route(p -> p.path("/classiccrew/payment-service/**")
+                .filters(f -> f
+                        .rewritePath("/classiccrew/payment-service/(?<segment>.*)", "/${segment}")
+                        .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                .uri("lb://PAYMENT-SERVICE")).build();
+    }
+}
