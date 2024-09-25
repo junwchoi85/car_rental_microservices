6d0a4e475be4d5fe04a711106ed608ae930f64c9
Jun Hwan <junwchoi85@gmail.com>
2024-09-26 04:19:21 +1200

    Introducint Car Listing Service

---
+++ car-listing-service/src/main/java/io/classiccrew/car_listing_service/CarListingServiceApplication.java
@@ -0,0 +1,13 @@
+package io.classiccrew.car_listing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarListingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarListingServiceApplication.class, args);
+	}
+
+}
---
+++ car-listing-service/src/test/java/io/classiccrew/car_listing_service/CarListingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.classiccrew.car_listing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarListingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
